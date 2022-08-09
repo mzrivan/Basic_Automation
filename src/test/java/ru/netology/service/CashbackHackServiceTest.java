e2e22c609db0f3106ce6ff46a4444ec05ce3f3c6
mzrivan <mzr993@gmail.com>
2022-08-09 22:07:30 +0300

    JUnit Jupiter added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,7 @@
 package ru.netology.service;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
@@ -16,6 +18,26 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void CalculateCashback1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CalculateCashback999JU5() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void CalculateCashback1000JU5() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
